be396c5d6105405eeb3a5741580c3f3e3ec87476
care4u <2669624618@qq.com>
2025-11-09 21:06:11 +0800

    m

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@
           <span class="close" @click="showDownloadModal = false">&times;</span>
           <a>下载 ZError for Windows</a>
           <div class="download-options">
-            <a href="https://cdn.zerror.cc/ZError_Setup_2.0.0.exe" class="download-btn direct-download">
+            <a href="https://cdn.zerror.cc/ZError_Setup_2.0.1.exe" class="download-btn direct-download">
               <i class="icon-download"></i> 直链下载
             </a>
             <a href="https://wwyl.lanzoum.com/b00ocrzzje" class="download-btn cloud-download">
