214667e99fb9bf0e4133ca83e3af355dba0c2a32
care4u <2669624618@qq.com>
2025-11-18 16:46:11 +0800

    m

---
+++ dist/assets/ChangelogView-BJKMJEFW.js
@@ -0,0 +1,19 @@
+import{_ as a,c as n,a as e,F as d,r as c,o,t}from"./index-DKVxYjJF.js";const l={data(){return{logs:[{version:"2.0.2",date:"2025-11-18",description:`1. 修复某些情况下需要管理员身份运行的问题
+2.修复一些默认模型的无法使用的问题
+3.优化题库页对图片的显示
+4.更好的AI请求错误处理`},{version:"2.0.1",date:"2025-11-8",description:"1. 修复更新后请求500错误的问题"},{version:"2.0.0",date:"2025-11-8",description:`1. 改为Tauri架构，更好的界面
+2. 支持自定义平台与AI模型
+3. 支持文本推理模型
+4. 修复服务器无法开启的bug`},{version:"1.4.0",date:"2025-06-25",description:`1. 截图搜题支持markdown以及latex语法显示
+2. 添加火山引擎以及FreeQwQ
+3. 优化软件启动与关闭`},{version:"1.3.1",date:"2025-06-19",description:`1.修复局域网开放功能
+2.修复再添加ai回答到数据库后，文件夹树题目统计不刷新的问题
+3.修复UI过大问题
+4.修复多选题目时，右键菜单问题`},{version:"1.3.0",date:"2025-05-18",description:`1.添加题库分类功能
+ 2.添加图片url显示功能
+ 3. 题库的精确匹配改为模糊匹配
+ 4.可以设置服务端口，并且对局域网开放
+ 5.美化界面`},{version:"1.2.0",date:"2025-05-2",description:`1. 添加截图搜题
+2. 修复题库无法修改的问题`},{version:"1.1.2",date:"2025-04-21",description:`1. 优化提示词
+2. 修复删除题目失败的问题`},{version:"1.1.1",date:"2025-03-21",description:"1. 修复密钥保存问题"},{version:"1.1.0",date:"2025-03-21",description:`1. 批量导入问题
+2. 修复文件权限问题`},{version:"1.0.0",date:"2025-03-20",description:"首次发布版本"}]}}},p={class:"changelog-page"},v={class:"changelog-container"},_={class:"version-badge"},g={class:"log-content"},u={class:"log-date"},h={class:"log-description"};function f(k,i,x,I,r,b){return o(),n("div",p,[i[0]||(i[0]=e("div",{class:"changelog-header"},[e("h1",null,"更新日志"),e("p",{class:"subtitle"},"ZError版本更新记录")],-1)),e("div",v,[(o(!0),n(d,null,c(r.logs,s=>(o(),n("div",{key:s.version,class:"log-card"},[e("div",_,t(s.version),1),e("div",g,[e("div",u,t(s.date),1),e("div",h,t(s.description),1)])]))),128))])])}const w=a(l,[["render",f],["__scopeId","data-v-38101e73"]]);export{w as default};
