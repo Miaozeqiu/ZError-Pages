58c38b4b7809fe1de795b3b4c64691e7ad3861a8
care4u <2669624618@qq.com>
2026-03-31 17:17:20 +0800

    更新至2.2.1

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@
           <span class="close" @click="showDownloadModal = false">&times;</span>
           <a>下载 ZError for Windows</a>
           <div class="download-options">
-            <a href="https://app.zerror.cc/apps/ZError_Setup_2.2.0.exe" class="download-btn direct-download">
+            <a href="https://app.zerror.cc/apps/ZError_Setup_2.2.1.exe" class="download-btn direct-download">
               <i class="icon-download"></i> 直链下载
             </a>
             <a href="https://wwyl.lanzoum.com/b00ocrzzje" class="download-btn cloud-download">
